0b13645adc73dc39e640aa2d1f0195ade31a630e
mmulyana <mulyan.t20@gmail.com>
2023-08-06 11:51:48 +0700

    update

--- src/component/atoms/Signal/configs.ts
+++ src/component/atoms/Signal/configs.ts
@@ -1,13 +1,16 @@
 const configs = {
   bg: {
     primary: 'bg-[#1DAF58]/20',
+    info: 'bg-[#3d4aff]/20',
   },
   color: {
     primary: 'bg-[#1DAF58]',
+    info: 'bg-[#3d4aff]',
   },
   shadow: {
-    primary: 'shadow-[0_0_20px_4px_rgba(29,175,88,0.32)]'
-  }
+    primary: 'shadow-[0_0_20px_4px_rgba(29,175,88,0.32)]',
+    info: 'shadow-[0_0_20px_4px_rgba(61,74,255,0.32)]',
+  },
 }
 
 export default configs
